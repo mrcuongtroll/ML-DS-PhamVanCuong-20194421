ba421390ef3e229a7650e0452534a5ed851e314c
Cuong <mrcuongtroll@gmail.com>
2021-04-18 22:11:10 +0700

    added tf-idf exercise

--- Session 1/LinearRegression.py
+++ Session 1/LinearRegression.py
@@ -16,8 +16,8 @@ def get_data(path, comment_symbol = '#'):
 
 def normalize_and_add_ones(X):
     X = np.array(X)
-    X_max = np.array([np.amax(X[:, column_id]) for column_id in range(X.shape[1])])
-    X_min = np.array([np.amin(X[:, column_id]) for column_id in range(X.shape[1])])
+    X_max = np.array([[np.amax(X[:, column_id]) for column_id in range(X.shape[1])] for _ in range(X.shape[0])])
+    X_min = np.array([[np.amin(X[:, column_id]) for column_id in range(X.shape[1])] for _ in range(X.shape[0])])
     X_normalized = (X - X_min)/(X_max - X_min)
     ones = np.array([[1] for _ in range(X_normalized.shape[0])])
     return np.column_stack((ones, X_normalized))
