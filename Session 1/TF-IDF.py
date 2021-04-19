2f51404c712cda132b6dc5e9bc78933a77035ca9
Cuong <mrcuongtroll@gmail.com>
2021-04-19 14:05:38 +0700

    added 20newsgroups-data-tf-idf.txt because I forgot last time

--- Session 1/TF-IDF.py
+++ Session 1/TF-IDF.py
@@ -98,11 +98,13 @@ def get_tf_idf(data_path):
         words_tfidfs_normalized = [str(index) + ':' + str(tf_idf_value/np.sqrt(sum_squares)) for index, tf_idf_value in words_tfidfs]
         sparse_rep = ' '.join(words_tfidfs_normalized)
         data_tf_idf.append((label, doc_id, sparse_rep))
+    with open('../datasets/20news-bydate/20newsgroups_data_tf_idf.txt', 'w') as f:
+        f.write('\n'.join([str(label) + '<fff>' + str(doc_id) + '<fff>' + sparse_rep for label, doc_id, sparse_rep in data_tf_idf]))
     return data_tf_idf
 
 #Try running 'em all
 if __name__ == '__main__':
-    gather_20newsgroups_data()
-    generate_vocabulary('../datasets/20news-bydate/20news-full-processed.txt')
+    # gather_20newsgroups_data()
+    # generate_vocabulary('../datasets/20news-bydate/20news-full-processed.txt')
     data_tf_idf_full = get_tf_idf('../datasets/20news-bydate/20news-full-processed.txt')
     print(data_tf_idf_full[69])
